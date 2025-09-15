25c8201510993acc86615108f88247723a4b6a0c
Sullivan Marín <sullivan.marin20@gmail.com>
2025-09-15 10:29:38 +0100

    Added form to create clients

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,13 @@
 const api = "http://localhost:8080/api/clients";
 
+const createClientForm = document.getElementById("createClientForm");
+
 async function fetchClients() {
     try{
         const response = await fetch(api);
         const clients = await response.json();
         const clientList = document.getElementById("ClientList");
+        clientList.innerHTML = "";
 
         clients.forEach(client => {
             const li = document.createElement("li");
@@ -15,5 +18,33 @@ async function fetchClients() {
         console.error("Error fetching clients:", error);
     }
 }
+
+async function createClient(Client) {
+    try {
+        const response = await fetch(api, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(Client)
+        });
+        if (response.ok) {
+            fetchClients();
+        }
+    } catch (error) {
+        console.error("Error creating client:", error);
+    }
+}
+
+createClientForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const address = document.getElementById("address").value;
+    const newClient = { name, email, phoneNumber, address };
+    createClient(newClient);
+    this.reset();
+});
 
 fetchClients();
